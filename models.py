771f5ce38a3ac95c995efcdef3870cd64f69c83f
andreivisan <andrei.m.visan@gmail.com>
2021-11-17 09:52:28 +0100

    Added the models

---
+++ models.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+
+
+class Tag(BaseModel):
+    id: Optional[UUID] = uuid4
+    label: str
+
+
+class Note(BaseModel):
+    id: Optional[UUID] = uuid4
+    label: str
+
+
+class Book(BaseModel):
+    id: Optional[UUID] = uuid4
+    title: str
+    read_date: date
+    tags: List[Tag]
+    notes: List[Note]
+
+
+class Role(BaseModel):
+    admin: "admin"
+    user: "user"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    name: str
+    username: str
+    email: str
+    password: str
+    books: List[Book]
+    roles: List[Role]
